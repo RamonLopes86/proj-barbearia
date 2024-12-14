2c92468460f60d620af79d482d8bc934549fca99
Ramon Lopes <dev.ramondasilvalopes@gmail.com>
2024-12-13 23:24:58 -0300

    ajuste do middleware

---
+++ src/_middleware.js
@@ -0,0 +1,19 @@
+// import {  NextResponse } from "next/server";
+
+
+// export function middleware(request){
+
+   
+        
+
+//         if(request.nextUrl.pathname.startsWith('/pagelogado')){
+
+//            return  NextResponse.redirect(new URL('/', request.url))
+
+//         }
+
+
+//         return  NextResponse.next()
+   
+
+// }
